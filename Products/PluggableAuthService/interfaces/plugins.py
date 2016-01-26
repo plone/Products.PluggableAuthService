b08324942a8a9d911d2d8035756048295160c8f4
Jens Vagelpohl <jens@alias.ooo>
2016-01-26 17:06:22 +0100

    - move interface IGroupManagement to PluggableAuthService

--- Products/PluggableAuthService/interfaces/plugins.py
+++ Products/PluggableAuthService/interfaces/plugins.py
@@ -556,3 +556,43 @@ class IGroupIntrospection(Interface):
         """
         return the members of the given group
         """
+
+
+class IGroupManagement(Interface):
+
+    def addGroup(id, **kw):
+        """
+        Create a group with the supplied id, roles, and groups.
+        return True if the operation suceeded
+        """
+
+    def addPrincipalToGroup(principal_id, group_id):
+        """
+        Add a given principal to the group.
+        return True on success
+        """
+
+    def updateGroup(id, **kw):
+        """
+        Edit the given group. plugin specific
+        return True on success
+        """
+
+    def setRolesForGroup(group_id, roles=()):
+        """
+        set roles for group
+        return True on success
+        """
+
+    def removeGroup(group_id):
+        """
+        Remove the given group
+        return True on success
+        """
+
+    def removePrincipalFromGroup(principal_id, group_id):
+        """
+        remove the given principal from the group
+        return True on success
+        """
+
